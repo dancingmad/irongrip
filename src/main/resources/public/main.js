0b4f0d3f4df3a82436cbbb372cd95d3f093d263f
thorstenreis <thorsten.reis@gmail.com>
2018-12-02 12:13:07 +0100

    bumping the version to 1.0

--- src/main/resources/public/main.js
+++ src/main/resources/public/main.js
@@ -2459,7 +2459,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/thorstenreis/IdeaProjects/irongrp/src/main/angular/mnemotica/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /Users/thorstenreis/Documents/dev/irongrp/src/main/angular/mnemotica/src/main.ts */"./src/main.ts");
 
 
 /***/ })
--- src/main/resources/public/styles.js
+++ src/main/resources/public/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/thorstenreis/IdeaProjects/irongrp/src/main/angular/mnemotica/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /Users/thorstenreis/Documents/dev/irongrp/src/main/angular/mnemotica/src/styles.css */"./src/styles.css");
 
 
 /***/ })
